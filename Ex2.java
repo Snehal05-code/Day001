cc02be487353b9732d3e3d96e88f8145622bddf6
Snehal05-code <snehal.dhawale24@pcu.edu.in>
2025-01-05 23:20:25 +0530

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,26 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        int a=7, b=4;
+        //+ for addition
+        int c=a+b;
+        System.out.println(c);
+        //- for subtraction
+        int d=a-b;
+        System.out.println(d);
+        //* for multiplication
+        int e=a*b;
+        System.out.println(e);
+        // / for division
+        int f=a/b;
+        System.out.println(f);
+        // % for remainder
+        int g=a%b;
+        System.out.println(g);
+         int x=10,y=10;
+         int p,q;
+         p=x++;
+         q=++y;
+         System.out.println("p="+p+",q="+q);
+        System.out.println("x="+x+",y="+y);
+    }
+}
