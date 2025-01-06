58e5006f5664ed370a828c4b4cb96e10095f0ca0
Snehal05-code <snehal.dhawale24@pcu.edu.in>
2025-01-06 23:21:10 +0530

    Update Breakconti.java

--- Breakconti.java
+++ Breakconti.java
@@ -1,12 +1,19 @@
+//Defines the class for the program
 public class Breakconti {
+    // The main method of the program.
     public static void main(String[] args) {
+         // Initialize the variable 'i'
         int i=1;
+        //Start the loop till 'i'is less then or equal to 10
         while (i<=10) {
             System.out.println(i);
+             // Check if the value of 'i'
             if (i==5) {
+                // Exit the loop immediately if 'i=5'
                 break;
             }
             i++;
+            //Skip the rest part of loop
                continue;
         }
 
