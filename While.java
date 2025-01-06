f00dc1ece1310502d69a398e273a334f70cb8b93
Snehal05-code <snehal.dhawale24@pcu.edu.in>
2025-01-07 01:02:13 +0530

    Update While.java

--- While.java
+++ While.java
@@ -1,8 +1,14 @@
+//Define a public class While. the class demonstrates the use of a while loop
 public class While {
+    //The main method for program execution.
     public static void main(String[] args) {
+        //Initialize the integer variable 'cout' with yhe value 1
         int count = 1;
+        
         while (count <= 5) {
+            //Print the String 'hello'
             System.out.println("hello");
+           //Increment  the 'count' by 1
             count++;
         }
     }
